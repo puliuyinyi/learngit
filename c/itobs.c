fef4527cb6282bc42ef166ec32fcac1195a97bdd
puliuyinyi <wangmeng_email@126.com>
2014-04-22 21:31:42 +0800

    create a c function itobs: interget to binary string

---
+++ c/itobs.c
@@ -0,0 +1,12 @@
+char * itobs(int n,char *ps)
+{
+	int i;
+	static int size = 8 * sizeof(int);
+	for(i = size - 1;i >= 0;i--)
+	{
+		ps[i] = (01 & n) +'0';
+		n >>= 1;
+	}
+	ps[size] = '\0';
+	return ps;
+}
